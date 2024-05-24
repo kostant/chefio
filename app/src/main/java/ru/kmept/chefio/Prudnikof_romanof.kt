227278eb9c65d3c30aca7ed54f9a0794ee9bbee3
Студент <student@imes.local>
2024-05-24 18:30:45 +0300

    New version

--- app/src/main/java/ru/kmept/chefio/Prudnikof_romanof.kt
+++ app/src/main/java/ru/kmept/chefio/Prudnikof_romanof.kt
@@ -3,11 +3,10 @@ package ru.kmept.chefio
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class prudnikov : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.prudnikov_romanov)
 
     }
 }
