500cd3c23cbb4a1d6a450e3cb904694f4568b4b5
kmept <kmept@gmail.com>
2024-05-24 18:41:11 +0300

    много чего

---
+++ app/src/test/java/PasswordRecoveryActivity.kt
@@ -0,0 +1,14 @@
+package com.example.yourapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.kmept.chefio.R
+
+class PasswordRecoveryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_recovery)
+
+    }
+}
