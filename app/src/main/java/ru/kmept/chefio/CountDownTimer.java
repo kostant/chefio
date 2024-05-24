7fb04ec039c68108965a0c50be5c8aef468fbfb2
Smile <mralkatraz49@gmail.com>
2024-05-24 19:07:16 +0300

    One

---
+++ app/src/main/java/ru/kmept/chefio/CountDownTimer.java
@@ -0,0 +1,12 @@
+package ru.kmept.chefio;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CountDownTimer {
+    public CountDownTimer(long totalTimeInMillis, int i) {
+    }
+}
+
+
