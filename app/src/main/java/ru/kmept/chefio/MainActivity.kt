954c61dc57f8e1973a961846b40d416fca1f3544
Konstantin Antonchik <kostant12@gmail.com>
2024-05-24 16:19:32 +0300

    initial

---
+++ app/src/main/java/ru/kmept/chefio/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.kmept.chefio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
