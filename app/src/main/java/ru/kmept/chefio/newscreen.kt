7cb2c8233eb2dc062f1d1ee91ca7096ad57b6856
Kurepin <Fkurepin1703@gmail.com>
2024-05-25 14:21:15 +0300

    25.05

--- app/src/main/java/ru/kmept/chefio/newscreen.kt
+++ app/src/main/java/ru/kmept/chefio/newscreen.kt
@@ -1,4 +1,11 @@
 package ru.kmept.chefio
 
-class newscreen {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class newscreen: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.newscreen)
+    }
 }
