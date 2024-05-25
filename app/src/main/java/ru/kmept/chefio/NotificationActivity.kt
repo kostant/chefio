75aaa16132b21a35d64f96910c3e2ff8ac82aa00
hasan090812 <13971397qwerty@gmail.com>
2024-05-25 14:27:11 +0300

    vasyamaga

--- app/src/main/java/ru/kmept/chefio/NotificationActivity.kt
+++ app/src/main/java/ru/kmept/chefio/NotificationActivity.kt
@@ -1,14 +1,12 @@
 package ru.kmept.chefio
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class NotificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
