57c73bdba27a5a38a18690c663349e18daf3f362
Erbol <sh1su19@yandex.ru>
2024-05-25 14:23:41 +0300

    fayzik

--- app/src/main/java/ru/kmept/chefio/recipeActivity.kt
+++ app/src/main/java/ru/kmept/chefio/recipeActivity.kt
@@ -1,11 +1,11 @@
 package ru.kmept.chefio
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class recipeAcrivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_recipe)
     }
 }
