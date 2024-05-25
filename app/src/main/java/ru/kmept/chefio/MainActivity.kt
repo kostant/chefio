9568604c9bfb128b6364a2caa882872be1c8f699
Студент <student@imes.local>
2024-05-25 14:09:59 +0300

    Go to reg

--- app/src/main/java/ru/kmept/chefio/MainActivity.kt
+++ app/src/main/java/ru/kmept/chefio/MainActivity.kt
@@ -1,13 +1,19 @@
 package ru.kmept.chefio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import ru.kmept.chefio.singUp.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.prudnikov_romanov)
-
-
+    }
+    // Метод для обработки клика на текст "Sign Up"
+    fun onSignUpClick(view: View) {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/ru/kmept/chefio/Prudnikof_romanof.kt
+++ app/src/main/java/ru/kmept/chefio/Prudnikof_romanof.kt
@@ -1,12 +1,23 @@
 package ru.kmept.chefio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import ru.kmept.chefio.singUp.SignUpActivity
 
 class prudnikov : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.prudnikov_romanov)
+
+
+        // Метод для обработки клика на текст "Sign Up"
+        fun onSignUpClick(view: View) {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
