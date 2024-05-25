83869508539351af1bc086f1615f7791037cd268
Студент <student@imes.local>
2024-05-25 12:24:53 +0300

    sdrasti

--- app/src/main/java/ru/kmept/chefio/CreateActivity.kt
+++ app/src/main/java/ru/kmept/chefio/CreateActivity.kt
@@ -3,7 +3,7 @@ package ru.kmept.chefio
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
