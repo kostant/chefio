526b20e445aff3c1c3cf9c93f5bbf1e9f660dc40
Студент <student@imes.local>
2024-05-31 17:41:23 +0300

    dobavlen create first step

--- app/src/main/java/ru/kmept/chefio/MainActivity.kt
+++ app/src/main/java/ru/kmept/chefio/MainActivity.kt
@@ -3,7 +3,8 @@ package ru.kmept.chefio
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/ru/kmept/chefio/NotificationActivity.kt
+++ app/src/main/java/ru/kmept/chefio/NotificationActivity.kt
@@ -1,12 +1,19 @@
 package ru.kmept.chefio
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class NotificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    public fun on_click(view: View)
+    {
+    var i =Intent(this,creare_first_step::class.java)
+        startActivity(i)
     }
 }
