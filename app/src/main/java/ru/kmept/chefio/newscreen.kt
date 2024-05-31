032802fb0bfbad36520fe35ffc7204fe310945ae
Kurepin <Fkurepin1703@gmail.com>
2024-05-31 17:16:17 +0300

    ssss

--- app/src/main/java/ru/kmept/chefio/newscreen.kt
+++ app/src/main/java/ru/kmept/chefio/newscreen.kt
@@ -1,11 +1,18 @@
 package ru.kmept.chefio
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class newscreen: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.newscreen)
+    }
+    public fun on_click_next(view:View)
+    {
+        var i:Intent = Intent(this, CreateActivity::class.java)
+        startActivity(i);
     }
 }
