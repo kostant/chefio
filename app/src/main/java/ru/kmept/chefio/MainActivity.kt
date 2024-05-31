a2f736637ad956bba1ccda6e57ae05b4b7dec470
Konstantin Antonchik <kostant12@gmail.com>
2024-05-31 18:11:48 +0300

    add fragment to tabs

--- app/src/main/java/ru/kmept/chefio/MainActivity.kt
+++ app/src/main/java/ru/kmept/chefio/MainActivity.kt
@@ -1,13 +1,16 @@
 package ru.kmept.chefio
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.framentsContainer, RecipeDetailsFragment())
+            .commit()
     }
 }
