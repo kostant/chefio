3dda452ac7be648503e5c17e4242bf3165ec4ec4
GhostArnold <134621808+GhostArnold@users.noreply.github.com>
2024-05-31 17:24:20 +0300

    Update MainActivity.kt

--- app/src/main/java/ru/kmept/chefio/MainActivity.kt
+++ app/src/main/java/ru/kmept/chefio/MainActivity.kt
@@ -3,17 +3,11 @@ package ru.kmept.chefio
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import ru.kmept.chefio.singUp.SignUpActivity
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.prudnikov_romanov)
-    }
-    // Метод для обработки клика на текст "Sign Up"
-    fun onSignUpClick(view: View) {
-        val intent = Intent(this, SignUpActivity::class.java)
-        startActivity(intent)
+        setContentView(R.layout.activity_main)
     }
 }
