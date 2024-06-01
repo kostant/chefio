5cf62d7505a180feb6e6c6cf3a8effe02dcc4e29
hasan090812 <13971397qwerty@gmail.com>
2024-06-01 17:21:47 +0300

    asd

--- app/src/main/java/ru/kmept/chefio/MainActivity.kt
+++ app/src/main/java/ru/kmept/chefio/MainActivity.kt
@@ -1,6 +1,10 @@
 package ru.kmept.chefio
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,4 +17,13 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.framentsContainer, RecipeDetailsFragment())
             .commit()
     }
-}
+
+    fun onNotificationsClick(view: View) {
+        val button: LinearLayout = findViewById(R.id.onNotificationsClickID)
+        button.setOnClickListener {
+            val intent = Intent(this, UsersActivity::class.java)
+            startActivity(intent)
+
+
+        }
+    }}
