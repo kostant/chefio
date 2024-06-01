293dad47805289178364f3c18cabd99f3de59f53
Студент <student@imes.local>
2024-06-01 15:23:22 +0300

    abc

--- app/src/main/java/ru/kmept/chefio/Prudnikof_romanof.kt
+++ app/src/main/java/ru/kmept/chefio/Prudnikof_romanof.kt
@@ -4,13 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import ru.kmept.chefio.singUp.SignUpActivity
 
 class prudnikov : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.prudnikov_romanov)
+        setContentView(R.layout.autorization_activity)
 
 
         // Метод для обработки клика на текст "Sign Up"
