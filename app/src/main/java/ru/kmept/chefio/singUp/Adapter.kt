acce6fb4b130e32afb25bdf3d93cf7404b61bee6
sh1su19@yandex.ru <@rHDvfdZAcq3>
2024-06-01 17:22:37 +0300

    asdf

---
+++ app/src/main/java/ru/kmept/chefio/singUp/Adapter.kt
@@ -0,0 +1,4 @@
+package ru.kmept.chefio.singUp
+
+class Adapter: Adapter<userViewHolder>()  {
+}
---
+++ app/src/main/java/ru/kmept/chefio/singUp/UserViewHolder.kt
@@ -0,0 +1,9 @@
+package ru.kmept.chefio.singUp
+
+import android.view.View
+import android.widget.AdapterView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder as ViewHolder1
+
+class UserViewHolder(itemView: View):ViewHolder1(itemView) {
+}
