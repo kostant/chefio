8531c17c66b69bb7d77a26848d0ccb14dce015a8
Студент <student@imes.local>
2024-06-01 17:21:16 +0300

    soldklik

---
+++ app/src/main/java/ru/kmept/chefio/singUp/SearchActivity.kt
@@ -0,0 +1,12 @@
+package ru.kmept.chefio.singUp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.kmept.chefio.R
+
+class SearchActivity : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.search_screen)
+    }
+}
