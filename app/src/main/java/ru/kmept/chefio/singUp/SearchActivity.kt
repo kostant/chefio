c8255edbd48aaa497802a774f9ac173c3adf0944
Студент <student@imes.local>
2024-06-07 18:14:31 +0300

    soldklick

--- app/src/main/java/ru/kmept/chefio/singUp/SearchActivity.kt
+++ app/src/main/java/ru/kmept/chefio/singUp/SearchActivity.kt
@@ -1,12 +1,27 @@
 package ru.kmept.chefio.singUp
 
+import SearchHistoryAdapter
+import android.annotation.SuppressLint
+import android.app.SearchManager
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ru.kmept.chefio.R
 
-class SearchActivity : AppCompatActivity()  {
+class SearchActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: SearchHistoryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.search_screen)
+        setContentView(R.layout.search_screen) // inflate the layout
+
+        recyclerView = findViewById(R.id.search_history_recyclerview) // now you can find the view
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        adapter = SearchHistoryAdapter()
+        recyclerView.adapter = adapter
     }
 }
---
+++ app/src/main/java/ru/kmept/chefio/singUp/SearchHistory.kt
@@ -0,0 +1,4 @@
+package ru.kmept.chefio.singUp
+
+data class SearchHistoryItem(val query: String, val timestamp: Long)
+
