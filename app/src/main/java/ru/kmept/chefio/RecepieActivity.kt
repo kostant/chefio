bf9851f06c54fda427b4064dd7a829680b005ace
513app <513app@mail.ru>
2024-06-21 18:46:26 +0300

    Recepies Connection Soldatenko

--- app/src/main/java/ru/kmept/chefio/RecepieActivity.kt
+++ app/src/main/java/ru/kmept/chefio/RecepieActivity.kt
@@ -2,6 +2,8 @@ package ru.kmept.chefio
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dalvik.system.ZipPathValidator
+import javax.security.auth.callback.Callback
 
 class RecepieActivity : AppCompatActivity() {
 
